5fb90332880f69cd5a2af0e281086db005ede403
Andre Alves <andre.alves@mesainc.com.br>
2026-03-04 11:46:53 -0300

    feat: add starWars api service

---
+++ src/main/java/br/com/alac/StarWarsResource.java
@@ -0,0 +1,24 @@
+package br.com.alac;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+@Path("starwars")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class StarWarsResource {
+
+    @RestClient
+    StarWarsService starWarsService;
+
+    @GET
+    @Path("starships")
+    public String getStarships() {
+        return this.starWarsService.getStarships();
+    }
+
+}
