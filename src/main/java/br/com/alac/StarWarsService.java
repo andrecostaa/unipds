2e2c8c9059c0b5e8d99d171b156bd46f8055379e
Andre Alves <andre.alves@mesainc.com.br>
2026-03-05 16:44:05 -0300

    feat: add poc about circuit breaker and timeout

--- src/main/java/br/com/alac/StarWarsService.java
+++ src/main/java/br/com/alac/StarWarsService.java
@@ -4,7 +4,12 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+
+import java.time.temporal.ChronoUnit;
 
 @RegisterRestClient(baseUri = "https://swapi.info/api/")
 public interface StarWarsService {
@@ -12,5 +17,23 @@ public interface StarWarsService {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Path("starships")
-    public String getStarships();
+    @Timeout(
+            value = 3,
+            unit = ChronoUnit.SECONDS
+    )
+    @Fallback(
+            fallbackMethod = "getStarshipsFallback"
+    )
+    @CircuitBreaker(
+            requestVolumeThreshold = 2,  //a cada 2 requests o circuit breaker vai analisar se precisa ativar o 'disjuntor'
+            failureRatio = .5, //50% , se de 2 requests 1 der erro, eu ativo o circuit
+            delay = 3000L, //3 seg
+            successThreshold = 2 //se der 2 requests de sucesso, desliga o disjuntor
+    )
+    public String getStarships(); //O metodo do fallback precisa ter a mesma assinatura do metodo que ta sendo aplicado
+
+
+    default String getStarshipsFallback() {
+        return "Fallback ";
+    }
 }
